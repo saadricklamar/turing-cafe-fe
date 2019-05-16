2d9a70fa053143933271d0277414d4ca62ae8ae3
Saad Baradan <saadbaradan@gmail.com>
2019-05-16 10:08:54 -0600

    Add reservation form and allow user to submit a reservation

---
+++ src/ReservationForm.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+
+
+class ReservationForm extends Component {
+    constructor() {
+        super()
+        this.state = {
+            name: '',
+            date: '',
+            time: '',
+            number: null
+        }
+    }
+
+    handleChange = e => {
+        const {name, value} = e.target;
+        this.setState({[name]: value})
+    }
+
+    handleSubmit = e => {
+        e.preventDefault();
+        this.props.addReservation(this.state)
+    }
+
+
+
+
+
+    render() {
+        return(
+            <form onSubmit={this.handleSubmit}>
+                <input type='text' name='name' onChange={this.handleChange}/>
+                <input type='text' name='date' onChange={this.handleChange}/>
+                <input type='text' name='time' onChange={this.handleChange}/>
+                <input type='text' name='number' onChange={this.handleChange}/>
+                <button>Submit</button>
+            </form>
+
+        )
+
+    }
+}
+
+export default ReservationForm
